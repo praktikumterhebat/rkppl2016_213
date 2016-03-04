8f5df38d0faf654ac76030c6cd00dfb6bc3ae736
eka windo <praktikumterhebat@gmail.com>
2016-03-04 14:16:38 +0700

    Ada yg baru

--- RKPPL/src/VersioningControl/Latihan.java
+++ RKPPL/src/VersioningControl/Latihan.java
@@ -5,6 +5,7 @@ public class Latihan {
 	public static void main(String []args){
 		System.out.println("Hello Praktikum :*");
 		System.out.println("????");
+		System.out.println("ini yg di ubah");
 	}
 
 }
